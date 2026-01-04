de25ab030691ea2751b9056727d208a3c860d8ef
SerkanEvin <serkan16evin16@gmail.com>
2026-01-04 23:57:12 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173,
+    port: 3000,
     strictPort: true,
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
